fbded03d46b520fc0e7df38f2287f1e14399515d
nabechin <nabenabe0258@gmail.com>
2020-12-28 16:59:03 +0900

    add: calc and test

---
+++ calc.py
@@ -0,0 +1,14 @@
+def add_num(num1: int, num2: int):
+    return num1 + num2
+
+
+def sub_num(num1: int, num2: int):
+    return num1 - num2
+
+
+def multi_num(num1: int, num2: int):
+    return num1 * num2
+
+
+def devide_num(num1: int, num2: int):
+    return num1 / num2
---
+++ tests/test_calc.py
@@ -0,0 +1,26 @@
+import pytest
+import calc
+
+
+def test_add():
+    num1 = 3
+    num2 = 5
+    assert calc.add_num(num1, num2) == 8
+
+
+def test_sub():
+    num1 = 3
+    num2 = 5
+    assert calc.sub_num(num1, num2) == -2
+
+
+def test_multi():
+    num1 = 3
+    num2 = 5
+    assert calc.multi_num(num1, num2) == 15
+
+
+def test_devide():
+    num1 = 3
+    num2 = 5
+    assert calc.devide_num(num1, num2) == 0.6
